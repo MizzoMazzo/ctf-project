d9059bb9381bc390f227da4c7c18559dff5a316f
MrMatch246 <s8daerce@stud.uni-saarland.de>
2020-01-15 11:16:19 +0100

    fixed input Varsize because strcmp only stops at NULL byte added option to remove generated source code after compiling. Added Converter that encodes binary with base85 (variable iterations ) and writes to file/ can use cmdline args

--- converter.py
+++ converter.py
@@ -1,26 +1,23 @@
-import binascii
-from base64 import b64encode
-from base64 import b32encode
 from base64 import b85encode
-
+import sys
 
 def reader(fname,iterations=1):
-    #encoded = None
+
     with open(fname, "rb") as binary_file:
         encoded = binary_file.read()
         for x in range(0,iterations):
             encoded=b85encode(encoded)
-    print(encoded)
-    print(len(encoded))
+        #print(encoded)
     if True:
         with open(fname+"encoded","wb") as outenc:
             outenc.write(encoded)
 
-
-
-
 def main():
-    reader("binary1")
+    if len(sys.argv)>1:
+        name=sys.argv[1]
+    else:
+        name="binary1"
+    reader(name)
     pass
 
 
