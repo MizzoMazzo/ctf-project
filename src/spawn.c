ee3eae8f9ed49286c748437876d0cde701334bf2
MizzoMazzo <devstuff20@gmail.com>
2020-01-21 13:54:30 +0100

    spawn.c

--- src/spawn.c
+++ src/spawn.c
@@ -26,7 +26,7 @@ void print_binary(){
   snprintf(filename, sizeof(filename), "binary%dencoded", getpid());
 
   fp = fopen(filename, "r");
-  printf("What is this??\n");
+  printf("What is this??\n\n");
   if (fp) {
       while ((i = getc(fp)) != EOF)
           putchar(i);
@@ -34,9 +34,31 @@ void print_binary(){
   }
 }
 
+void run_binary(){
+  char filename[15];
+
+  snprintf(filename, sizeof(filename), "./binary%d", getpid());
+  printf("\n");
+  system(filename);
+}
+
+void cleanup(){
+  char binaryfile[15];
+  char encodedfile[30];
+
+  snprintf(binaryfile, sizeof(binaryfile), "binary%d", getpid());
+  snprintf(encodedfile, sizeof(encodedfile), "binary%dencoded", getpid());
+
+  remove(binaryfile);
+  remove(encodedfile);
+}
+
 int main(){
   generate_binary();
   convert_binary();
   print_binary();
+  run_binary();
+  cleanup();
+
   return 0;
 }
